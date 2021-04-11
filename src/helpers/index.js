72795fe026cf9104179949959bcbf9e218a85544
gbenaimako <gbenimako@gmail.com>
2021-04-12 00:51:09 +0200

    netlify2

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -31,4 +31,4 @@ export function groupBy(key) {
     }, {});
   };
 }
-export const API_URL = `http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=${process.env.REACT_APP_KEY}&cnt=40`;
+export const API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=${process.env.REACT_APP_KEY}&cnt=40`;
