5a6a490b2bfbad64104575c42c3eb917f8d165e7
gbenaimako <gbenimako@gmail.com>
2021-04-11 11:43:33 +0200

    remove unavailable export

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 export { WeatherBox } from "./WeatherBox";
 export { LoadingScreen } from "./Loading";
 export { VerticalChart } from "./VerticalChart";
-export { Carousel } from "./Carousel";
 export { CheckBox } from "./CheckBox";
 export { Arrows } from "./Arrows";
 export { WeatherCard } from "./Card";
